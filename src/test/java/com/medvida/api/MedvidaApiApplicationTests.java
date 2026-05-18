6b3202ad30ebf450b076b2fb8e5b79837ceb0822
Bernardo da Silva <bsilvaaoliveira@gmail.com>
2026-05-18 10:02:40 -0300

    feat: Estrutura inicial do projeto com Spring Initializr

---
+++ src/test/java/com/medvida/api/MedvidaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medvida.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedvidaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
